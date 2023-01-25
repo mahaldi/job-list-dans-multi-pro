693ee85bc0944da1156abd4ddfd0db0e08b1e1ea
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-25 15:20:13 +0700

    add filter event func

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,29 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Grid from '@mui/material/Unstable_Grid2';
 import { TextField, Switch, Button } from '@mui/material'
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { WrapperFilter } from './style'
 
-export const Filter = () => {
+export const Filter = props => {
+	const { onSearch } = props
+	const [desc, setDesc] = useState('')
+	const [location, setLocation] = useState('')
+	const [fulltime, setFulltime] = useState(true)
 	return (
 		<>
 			<WrapperFilter>
-				<Grid container direction="row" spacing={2}>
+				<Grid container direction="row" spacing={2} justifyContent="center" alignItems="center">
 					<Grid xs="auto">
-						<TextField label="Description" variant='outlined' />
+						<TextField label="Description" variant='outlined' value={desc} onChange={(e) => setDesc(e.target.value)}/>
 					</Grid>
 					<Grid xs="auto">
-						<TextField label="Location" variant='outlined' />
+						<TextField label="Location" variant='outlined' value={location} onChange={(e) => setLocation(e.target.value)}/>
 					</Grid>
 					<Grid xs="auto">
 						<FormControlLabel
-							control={<Switch color="primary" />}
+							onChange={() => setFulltime(!fulltime)}
+							control={<Switch color="primary" checked={fulltime}/>}
 							label="Full time"
 							labelPlacement="top"
 						/>
 					</Grid>
 					<Grid xs="auto">
-						<Button variant='contained'>Search</Button>
+						<Button variant='contained' onClick={() => onSearch(desc, location, fulltime)}>Search</Button>
 					</Grid>
 				</Grid>
 			</WrapperFilter>
