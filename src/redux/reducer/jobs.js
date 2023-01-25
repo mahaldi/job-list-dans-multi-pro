27987d767928eb747459b7226da4d43ae9ec1ff1
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-25 11:47:58 +0700

    fix data mapping job reducer

--- src/redux/reducer/jobs.js
+++ src/redux/reducer/jobs.js
@@ -1,11 +1,11 @@
 const jobs = (state = {}, { type, payload }) => {
 	switch(type) {
 		case 'LOAD_JOBS_LIST':
-			return { ...state, jobs: { loading: true, error: false }}
+			return { ...state, loading: true, error: false}
 		case 'UPDATE_JOBS_LIST':
-			return { ...state, jobs: { loading: false, error: false, data: payload }}
+			return { ...state, loading: false, error: false, data: payload}
 		case 'FAILED_JOBS_LIST':
-			return { ...state, jobs: { loading: false, error: true, data: { ...state.jobs.data } }}
+			return { ...state, loading: false, error: true, data: { ...state.jobs.data }}
 		default:
 			return state;
 	}
