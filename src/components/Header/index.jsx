dd20f29dfdba97a1d94a1315e45aaf6156442962
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-25 12:39:35 +0700

    header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export const Header = () => {
 	return (
-		<>header section</>
+		<>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position="fixed">
+					<Toolbar>
+						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+							Dans Multi Pro
+						</Typography>
+						<Button color="inherit">Login</Button>
+					</Toolbar>
+				</AppBar>
+			</Box>
+		</>
 	)
 }
