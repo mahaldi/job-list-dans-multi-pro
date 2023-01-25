e09e9f3f0b8c1737291c3cc34111f3940fe809f7
Mahaldi Wijaya Saputra <aldiwijayasaputra@gmail.com>
2023-01-25 12:39:20 +0700

    filter component

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import Grid from '@mui/material/Unstable_Grid2';
+import { TextField, Switch, Button } from '@mui/material'
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { WrapperFilter } from './style'
 
 export const Filter = () => {
 	return (
-		<>Filter section</>
+		<>
+			<WrapperFilter>
+				<Grid container direction="row" spacing={2}>
+					<Grid xs="auto">
+						<TextField label="Description" variant='outlined' />
+					</Grid>
+					<Grid xs="auto">
+						<TextField label="Location" variant='outlined' />
+					</Grid>
+					<Grid xs="auto">
+						<FormControlLabel
+							control={<Switch color="primary" />}
+							label="Full time"
+							labelPlacement="top"
+						/>
+					</Grid>
+					<Grid xs="auto">
+						<Button variant='contained'>Search</Button>
+					</Grid>
+				</Grid>
+			</WrapperFilter>
+		</>
 	)
 }
---
+++ src/components/Filter/style.js
@@ -0,0 +1,12 @@
+import { styled } from '@mui/material/styles';
+
+export const WrapperFilter = styled('div')`
+	position: sticky;
+	z-index: 10;
+	top: 64px;
+	left: 0;
+	right: 0;
+	background-color: white;
+	padding-top: 20px;
+	width: 100%;
+`
